7784e6b547049da05fd86d2d3b20acbd311844ed
Banh <banh0703@gmail.com>
2019-10-03 19:38:07 +1000

    Script files 501

---
+++ delC.sh
@@ -0,0 +1,64 @@
+#Name: Xuan Anh Nguyen
+#Student ID: 524789
+
+#Description: This is the script file to delete old C files
+#When user input no argument, the script will list all .c files
+#When user input 1 or more arguments, it will just list all those files
+#in the arguments.
+
+#Set the script to run on Bourne shell
+#! /bin/sh
+
+#Function to ask the user to decide if they want to remove the file or not
+function deleteFile ()
+{
+	file=$1
+	head -n 10 $file
+	echo -e "\nDelete file $file? (y/n): \c"
+	read answer 
+	case "$answer" in
+       [yY]*) rm -f $file
+       echo -e "File $file deleted.\n" ;;
+       [nN]*)
+       echo -e "File $file NOT deleted.\n" ;;
+           *)
+       echo -e "Invalid option.\n"
+	esac
+}
+
+echo -e "This script removes C files which you no longer want to keep.\n"
+echo -e "Here are the C file(s) under the current directory:\n"
+
+#Check the argument input from the user
+numberOfArguments=$#
+if [ $numberOfArguments -lt 1 ];
+#The user enter no argument. 
+then
+	if find . -name "*.c" 2>/dev/null | wc -l > 0;
+	#If the .c files exist in the current folder
+	then
+   	   echo *.c
+	   for file in *.c; do
+ 	      echo "Displaying first 10 lines of $file:"
+	      deleteFile $file
+	    done
+	#If the .c file is not exist in the current folder     
+	else
+   	   echo -e "No C files found.\n"
+	fi
+
+#The user enter more than 1 argument.
+else
+	#For each argument that user input.
+	for arg
+	do
+	   echo "Displaying first 10 lines of $arg:"
+	   #If the file exist.
+	   if [ -f $arg ] ; then
+	      deleteFile $arg
+	   #If the file does not exist  
+	   else
+	      echo -e "File $arg does not exist.\n"
+	   fi
+	done	
+fi
---
+++ setting1.sh
@@ -0,0 +1,142 @@
+#Name: Xuan Anh Nguyen
+#Student ID: 524789
+
+#Description: This script allows the user to add, view or 
+#delete a setting in a configuration file.
+
+#Set the script to run on Bourne shell
+#! /bin/sh
+
+
+#Function to add a new setting.
+function add_new_setting ()
+{
+	#Loop until the user correct setting a new variable.
+	loop=y
+	while [ $loop = y ]
+	do
+		echo -e "\nEnter setting (format: ABCD=abcd): \c"
+		read varAdd
+		#Check if variable exist
+	    checkVariableName=$(grep $varAdd config.txt) 
+		if [[ -z $checkVariableName ]];
+		then 
+			#If the user only press enter/return
+			if [[ -z $varAdd ]] ; 
+			then
+				echo -e "New setting not entered\n" ; continue ;
+				
+			#If the user enter only the variable name without "=" sign
+			elif [ $(expr index $varAdd '=') = 0 ] ; then
+				echo -e "Invalid setting\n"
+				
+			#If the user enter only the variable name with "=" sign	
+			elif [ 	${varAdd: -1} = "=" ] ; then
+				echo -e "The variable name of the setting is: ${varAdd%=*} " 
+				echo -e "The variable value of the setting is: "
+				echo -e "Invalid setting.\n" ; continue ;
+				
+			#If the user enter only enter the variable value with the "=" before variable value 	
+			elif [ ${varAdd:0:1} = "=" ] ; then
+				echo -e "The variable name of the setting is: "
+				echo -e "The variable value of the setting is: ${varAdd#*=}"
+				echo -e "Invalid setting.\n" ; continue ;
+				
+			#If the user enter both the variable name and value 
+			#but the first character is a number
+			elif [ ${varAdd:0:1} -eq ${varAdd:0:1} ] 2>/dev/null ; then
+				echo -e "The variable name of the setting is: ${varAdd%=*} "
+				echo -e "The variable value of the setting is: ${varAdd#*=}"
+				echo -e "Invalid setting. The first character of a variable name cannot be a digit.\n"
+			
+			#The user enter the correct new setting convention + Add new setting to config.txt
+			else
+				echo -e "The variable name of the setting is: ${varAdd%=*} "
+				echo -e "The variable value of the setting is: ${varAdd#*=}"
+				echo $varAdd >> config.txt
+				echo -e "New setting added.\n" ; 
+				break ;
+			fi 
+		else
+			echo -e "The variable name of the setting is: ${varAdd%=*} "
+			echo -e "The variable value of the setting is: ${varAdd#*=}"
+			echo -e "Variable exists. Changing the values of existing variables is not allowed." ; 
+			break ;
+		fi
+	done
+}
+
+#Main part of the script
+#Check if the config.txt file is exist
+if [[ ! -f config.txt ]]
+#If the file does not exist 
+then 
+   echo -e "The configuration file does not exist.\n"
+#If the file exists.   
+else
+   #Loop continuously the menu until quit.
+   answer=y
+   while [ $answer = y ]
+   do
+	  #print the menu option
+      echo -e "\n*** MENU ***"
+      echo -e "\n 1. Add a Setting"
+      echo -e "\n 2. Delete a Setting"
+      echo -e "\n 3. View a Setting"
+      echo -e "\n 4. View All Settings"
+      echo -e "\n Q. Q - Quit\n\n"
+      echo -e "CHOICE: \c"
+	  
+	  read choice
+	  case $choice in
+	  #user choose 1: add a new setting
+	  1) add_new_setting ;;	     
+	  
+	  #user choose 2: delete a setting
+	  2) echo -e "\nEnter Variable Name:\c"
+		 read variableName 
+		 #Check variable
+	     checkName=$(grep $variableName config.txt) 
+		 if [ -z $checkName ];
+		 then 
+			echo -e "\nVariable does not exist."
+		 else
+			echo -e "\n`grep $variableName config.txt`\n"
+			#Check if user want to delete variable or not.
+			echo -e "Delete This Setting (y/n)?\c" ;
+			read userInput ;
+			case $userInput in
+			#If the user choose yes delete the variable and save the result in new file newConfig.txt.
+			y|Y)sed /$variableName/d ./config.txt > newConfig.txt ;
+				#Replace the config.txt content with the content of the new file newConfig.txt
+				cat newConfig.txt > config.txt ; 
+				echo "Setting Deleted" ;;
+			  *) ;;
+			esac 
+		 fi ;;
+		 
+	  #user choose 3: display a setting
+	  3) echo -e "\nEnter Variable Name:\c"
+		 read variableName
+		 #Check variable
+	     checkName=$(grep $variableName config.txt) 
+		 if [[ -z $checkName ]];
+		 then 
+			echo -e "\nVariable does not exist."
+		 else
+			echo -e "\n`grep $variableName config.txt`\n"
+			echo -e "Requested Setting Displayed Above.\n"
+		 fi ;;
+	  
+	  #user choose 4: display all the settings
+	  4) echo -e "\n`cat config.txt`\n" ;;
+	  
+	  #user choose quit
+	  q|Q) echo -e "\nThank You!" ; break ;;
+	  
+	  #user choose an invalid option
+	  *) echo -e "\nInvalid Option\n" ;
+		 echo -e "\nPlease choose from 1 to 4 or q to quit!\n" ;;	  
+	  esac
+   done
+fi
